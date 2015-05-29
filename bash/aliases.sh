50edebd7b39682e01719fe4805343f84b3a2c074
Kiss Gabor <nevi@agmen.hu>
2015-05-29 14:43:51 +0200

    Bash aliases: better organization

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -1,11 +1,20 @@
-alias grep='grep --color=always'
-alias egrep='egrep --color=always'
-alias fgrep='fgrep --color=always'
-alias ll='ls -laX --color=always --group-directories-first'
-alias lll='ls -laX --color=never --group-directories-first | less'
-alias bdd='haxelib run bdd'
-
+# fancy navigation
 alias ..='cd ../..'
 alias ...='cd ../../..'
 alias ....='cd ../../../..'
 alias .....='cd ../../../../..'
+
+# grep colors
+alias grep='grep --color=always'
+alias egrep='egrep --color=always'
+alias fgrep='fgrep --color=always'
+
+# ls aliases
+alias la='ls -A'
+alias l='ls -CF'
+alias ll='ls -alXF --color=always --group-directories-first'
+# searchable view for big directories
+alias lll='ls -alXF --color=never --group-directories-first | less'
+
+alias bdd='haxelib run bdd'
+
