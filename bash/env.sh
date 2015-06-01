cb51173f5b0aa65920e2f6b3637a18e6cca7e0c3
Kiss Gabor <nevi@agmen.hu>
2015-06-01 18:37:54 +0200

    Bash: hh works by default as the original searcher

--- bash/env.sh
+++ bash/env.sh
@@ -6,4 +6,4 @@ export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
 
 # enable colors for hstr
 # promising history search app (https://github.com/dvorka/hstr)
-export HH_CONFIG=hicolor
+export HH_CONFIG=hicolor,keywords,rawhistory
